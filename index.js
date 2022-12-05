23354e44a2d0d12e7ccf07ac0c08e10346bb2783
tony555t <tonytullah764@gmail.com>
2022-12-05 18:02:55 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// MULTIPLY FUNCTION
 
-// Write your code here
+   const num1=2
+    const num2=31
+    const multiply= num1*num2
+    
+
+   var random= Math.floor(Math.random()*10)+1;
+   
+
+var num3=19;
+var num4=5;
+var mod;
+var mod=num3 % num4;
+
+var max=(1, 2, 3, 4, 5,20);
+Math.max(1, 2, 3, 4, 5,20);
